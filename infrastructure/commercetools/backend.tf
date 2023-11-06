f0e28e632939427b92f0ee05b28ea7d8171537c7
Roberto Losanno <roberto.losanno@applydigital.com>
2023-11-06 11:21:49 +0000

    fix: removed commented out code

--- infrastructure/commercetools/backend.tf
+++ infrastructure/commercetools/backend.tf
@@ -1,16 +1,9 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-# The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
-# For more information, see https://www.terraform.io/docs/backends/types/remote.html
 terraform {
   cloud {
     organization = "ApplyDigitalForEagleEye"
 
     workspaces {
-      #      project = "eagleeye-commercetools-plugin"
       tags = ["commercetools"]
-      #      prefix = "ee-ct-plugin-commercetools-"
     }
   }
 
