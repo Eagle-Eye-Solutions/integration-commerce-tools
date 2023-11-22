380805c01127febce9149b32fdf90f25351c68be
Roberto Losanno <roberto.losanno@applydigital.com>
2023-11-22 23:37:03 +0000

    chore: added gcp loggin-winston

--- integration/src/config/logger.config.ts
+++ integration/src/config/logger.config.ts
@@ -2,23 +2,24 @@ import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
 import * as winston from 'winston';
 import { format, transports } from 'winston';
 import { APP_NAME } from '../common/constants/constants';
-// import { LoggingWinston } from '@google-cloud/logging-winston';
+import { LoggingWinston } from '@google-cloud/logging-winston';
 
-// const loggingWinston = new LoggingWinston();
+const loggingWinstonGCP = new LoggingWinston();
 
 export const loggerConfig = {
   transports: [
-    new transports.Console({
-      level: 'debug',
-      format: format.combine(
-        winston.format.timestamp(),
-        winston.format.ms(),
-        nestWinstonModuleUtilities.format.nestLike(APP_NAME, {
-          colors: true,
-          prettyPrint: true,
-        }),
-      ),
-    }),
-    // loggingWinston,
+    ['dev', 'test'].includes(process.env.NODE_ENV)
+      ? new transports.Console({
+          level: 'debug',
+          format: format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike(APP_NAME, {
+              colors: true,
+              prettyPrint: true,
+            }),
+          ),
+        })
+      : loggingWinstonGCP,
   ],
 };
